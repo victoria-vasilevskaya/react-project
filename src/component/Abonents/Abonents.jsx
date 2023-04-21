e9034dd4f12f352ae854ea36465de10a3d0346d2
Alexander Mukhanyuk <mukhanyuk04@gmail.com>
2023-04-21 23:48:20 +0300

    Добавил тег Abonents

---
+++ src/component/Abonents/Abonents.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from "./Abonents.module.css";
+
+function Abonents(){
+    return(
+        <div className={s.Abonents}>
+            <div className={s.tableAbonent}>
+
+            </div>
+        </div>
+    );
+}
+
+export default Abonents;
