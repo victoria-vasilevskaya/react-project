36d644ddc0081927689719fd5a129a41d835d9dd
kingofcutie <yanesobakaa@gmail.com>
2023-04-21 18:56:44 +0300

    Update app

---
+++ src/components/application-form.js
@@ -0,0 +1,14 @@
+
+function ApplicationForm() {
+    return (
+        <div className="application-form">
+            <div className="application-form-container">
+                <div>Caption</div>
+                <div>inputs</div>
+                <div>btn-save + btn-cancel</div>
+            </div>
+        </div>
+    )
+}
+
+export default ApplicationForm;
