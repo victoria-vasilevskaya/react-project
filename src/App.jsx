5ea82a100e0b3109e8a68697a328b6ebfc7b4f12
kingofcutie <yanesobakaa@gmail.com>
2023-04-28 01:33:12 +0300

    edit login.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import st from './App.module.css';
-import { Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import ApplicationForm from './components/application-form/application-form';
 import Login from './pages/login/login';
+import { Main } from './main';
 
 function App() {
   return (
     <div className={st.app}>
-      <Login />
+      <Main />
     </div>
-    /* <div className={st.app}>
-      <Header />
-      <div>
-        
-      </div>
-    </div> */
   );
 }
 
